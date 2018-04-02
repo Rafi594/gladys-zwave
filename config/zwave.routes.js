25e8a143b28944e97d37e333ba66b6cd6915f355
LeptitGeek <mxflash1996@gmail.com>
2018-04-02 14:18:25 +0200

    Modify routes

--- config/zwave.routes.js
+++ config/zwave.routes.js
@@ -1,8 +1,19 @@
-/**
- * Routes rules
- * @doc http://sailsjs.org/documentation/concepts/routes
- */
+/** 
+  * Gladys Project
+  * http://gladysproject.com
+  * Software under licence Creative Commons 3.0 France 
+  * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
+  * You may not use this software for commercial purposes.
+  * @author :: Mathieu Andrade
+  */
 
 module.exports.routes = {
+    
+    // Zwave
+    'post /zwave/addnode': 'ZwaveController.add',
+    'delete /zwave/removenode/:id': 'ZwaveController.remove',
+    'patch /zwave/setnodename': 'ZwaveController.setName',
+    'post /zwave/healnetwork': 'ZwaveController.heal',
   
-}
+  };
+  
