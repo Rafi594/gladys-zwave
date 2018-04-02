0da7fdb520b5230b1e6d040e0f943df53b65e0e8
LeptitGeek <mxflash1996@gmail.com>
2018-04-02 14:19:04 +0200

    Modify controller

--- controller/ZwaveController.js
+++ controller/ZwaveController.js
@@ -5,11 +5,35 @@
   * http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
   * You may not use this software for commercial purposes.
   * @author :: Mathieu Andrade
-  */
+*/
 
-prices = require('../external/edf.js');
-  
+addNode = require('../lib/addNode.js');
+removeNode = require('../lib/removeNode.js');
+setNodeName = require('../lib/setNodeName.js');
+healNetwork = require('../lib/healNetwork.js');
+
 module.exports = {
 
+  add: function(req, res, next){
+    addnode()
+      .then((result) => res.json(result))
+  },
+
+  remove: function(req, res, next){
+    removeNode({id: req.params.id})
+      .then((result) => res.json(result))
+  },
+
+  setName: function(req, res, next){
+    sails.log.debug("setName")
+    setNodeName(req.body)
+      .then((result) => res.json(result))
+  },
+
+  heal: function(req, res, next){
+    sails.log.debug("heal")
+    healNetwork()
+      .then((result) => res.json(result))
+  },
 
 }
